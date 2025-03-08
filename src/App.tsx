78b0fe5e7b960c19f468f803fc79712f45a08670
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2025-03-07 23:48:04 -0800

    Minimal auth0 without working backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,57 @@
+import React, { useState } from 'react';
 import { Route, Routes } from "react-router";
 import BudgetPage from "./pages/budgetpage.js"
 import HomePage from "./pages/homepage.js";
 import LoginPage from "./pages/loginpage.js";
 
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import envConfig from './Env';
+
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+  const [showLogin, setShowLogin] = useState(!isAuthenticated);
+
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+
+  const handleLogout = () => {
+    logout();
+    setShowLogin(true);
+  };
 
   return (
+  <>
     <Routes>
       <Route path="/budget" element={<BudgetPage/>} />
       <Route path="/" element={<HomePage/>}/>
       <Route path="/login" element={<LoginPage/>}/>
     </Routes>
+    <div>
+      {isAuthenticated ? (
+        <>
+          <p>Logged in as {user.name}</p>
+          <button onClick={handleLogout}>Logout</button>
+          <TodoList />
+        </>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )
+
+      }
+
+    </div>
+  </>
   );
 }
 
-export default App;
+export default () => (
+  <Auth0Provider
+    domain={envConfig.domain}
+    clientId={envConfig.clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+)
---
+++ src/Env.tsx
@@ -0,0 +1,7 @@
+const envConfig = {
+    domain: "dev-hoixp3jjti4nlbl0.us.auth0.com",
+    clientId: "z6qIeQlYrXfWAxLGSSEA6VaEN5kp8GUB"
+  };
+  
+export default envConfig;
+// module.exports = envConfig;
