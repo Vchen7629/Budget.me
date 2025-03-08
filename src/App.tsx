8674f8629985e9f39ccc5eef8a47adea07de3eb1
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2025-03-08 00:50:59 -0800

    Destroy unused loginpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Route, Routes } from "react-router";
 import BudgetPage from "./pages/budgetpage.js"
 import HomePage from "./pages/homepage.js";
-import LoginPage from "./pages/loginpage.js";
 
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 import envConfig from './Env';
@@ -13,7 +12,6 @@ function App() {
   const [showLogin, setShowLogin] = useState(!isAuthenticated);
 
   const handleLogin = () => {
-	  console.log("handleLogin entered!");
     loginWithRedirect();
   };
 
@@ -27,7 +25,6 @@ function App() {
     <Routes>
       <Route path="/budget" element={<BudgetPage/>} />
       <Route path="/" element={<HomePage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
     </Routes>
     <div>
       {isAuthenticated ? (
