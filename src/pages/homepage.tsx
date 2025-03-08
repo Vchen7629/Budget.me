64b282a78edb5ee9f2fe457d39e8426160ed7b6d
vchen7629 <chenvincent7629@gmail.com>
2025-03-08 01:52:08 -0800

    created component for adding new spending/Income stream

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -7,6 +7,8 @@ import GraphCard from '../components/GraphCard';
 import SpendingsCard from '../components/SpendingsCard';
 import { useNavigate } from 'react-router';
 import { LucideLogIn } from 'lucide-react';
+import { Toaster } from "@/components/ui/sonner"
+
 
 const Homepage = () => {
     const navigate = useNavigate();
@@ -16,6 +18,8 @@ const Homepage = () => {
     }
     return (
       <div className="min-h-screen bg-gray-100">
+                <Toaster richColors expand={true} className='z-50'/>
+
         <header className="flex items-center px-[2vw] justify-between bg-blue-600 text-white shadow-md">
           <div className="container mx-auto px-4 py-4">
             <h1 className="text-2xl font-bold">Budget App (name pending)</h1>
@@ -27,6 +31,7 @@ const Homepage = () => {
         </header>
 
         <main className="container mx-auto px-4 py-6">
+
           {/* left side menu on pc */}
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2 space-y-6">
@@ -44,7 +49,7 @@ const Homepage = () => {
           </div>
         </main>
 
-        <footer className="bg-gray-800 text-white py-4 mt-8">
+        <footer className="z-10 bg-gray-800 text-white py-4 mt-8">
           <div className="container mx-auto px-4 text-center text-sm">
             Budget App (name pending) &copy; 2025
           </div>
