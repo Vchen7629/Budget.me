2ce344aaa59d959cf9fbc11fa06b50d9eaa518eb
waffles-codes <evalu802@gmail.com>
2025-03-08 16:28:46 -0800

    lots of styling fixes

--- src/components/IncomeCard.tsx
+++ src/components/IncomeCard.tsx
@@ -19,9 +19,9 @@ const IncomeCard: React.FC = () => {
 
   
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
       {/* Income List */}
-      <div className="md:col-span-2 bg-white rounded-lg shadow-md p-4">
+      <div className="md:col-span-3 bg-white rounded-lg shadow-md p-4">
         <h2 className="text-xl font-bold mb-4">Income</h2>
         <div className="overflow-auto max-h-[500px]">
 
@@ -29,13 +29,13 @@ const IncomeCard: React.FC = () => {
       </div>
       
       {/* Add Income Panel */}
-      <div className="bg-white rounded-lg shadow-md p-4">
+      <div className="md:col-span-2 bg-white rounded-lg shadow-md p-4">
         <div className='flex justify-between'>
           <h2 className="text-lg font-bold mb-4">Add New Income</h2>
           <PeriodDropdownComponent />
         </div>  
         <div className="flex justify-between space-x-2">
-          <Input value={incomeValue} onChange={handleInputChange} type="amount" placeholder="Enter a new income stream" className="w-[82%] border-2 z-0 border-gray-400 text-gray-400"/>
+          <Input value={incomeValue} onChange={handleInputChange} type="amount" placeholder="Enter new income stream" className="w-[82%] border-2 z-0 border-gray-400 text-gray-400"/>
           <button onClick={handleAddNewIncome} className='flex items-center justify-center bg-green-400 w-[15%] rounded-lg'>
             <Plus className='text-white w-4'/>
           </button>
